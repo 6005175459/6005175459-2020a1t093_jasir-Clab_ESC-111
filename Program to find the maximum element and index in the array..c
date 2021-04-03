2e1443e13478b22a6717eed516bf846cbb84709a
6005175459 <75723060+6005175459@users.noreply.github.com>
2021-04-03 18:02:51 +0530

    Update Program to find the maximum element and index in the array..c

--- Program to find the maximum element and index in the array..c
+++ Program to find the maximum element and index in the array..c
@@ -1,22 +1,22 @@
-#include <stdio.h> //Pre-process directive to include standard input and output functions header file
-int main(){ //main function
-     int n, i, count = 0;
- 
-        printf("Enter the Number.\n"); //printf function calling
-        scanf("%d",&n);//This will scan (read) the number entered by user.
- 
-        for(i=2; i<=n/2; ++i){ //Declaration of a forloop
-    //Check for non prime number 
-        if(n%i==0){
-            count=1;
-            break;
-        }
-    }
-    //Declaration of if else statement
-    if (count==0)
-        printf("%d is a Prime number.",n);
-    else
-        printf("%d is not a Prime number.",n);
-    return 0; //return function
-    
+#include<stdio.h> 
+int main() 
+{  
+	int i;    
+	float arr[5];
+
+	printf("Please enter five numbers:\n ");
+
+	for (i = 0; i < 5; ++i) //for loop
+	{
+    scanf("%f", &arr[i]);
+	}
+
+		for (i = 1; i < 5; ++i) //for loop
+		{
+    	if (arr[0] < arr[i])
+        	arr[0] = arr[i];
+		}
+	printf("Largest element = %.2f", arr[0]);// This will give the result
+
+	return 0 ;
 }

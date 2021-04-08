06f8fb10b7278c9f5e58805815ce8a2e72b51b46
6005175459 <75723060+6005175459@users.noreply.github.com>
2021-04-08 09:24:41 +0530

    Update Program to calculate area and perimeter of circle, square and rectangle based on user’s choice..c

--- Program to calculate area and perimeter of circle, square and rectangle based on user’s choice..c
+++ Program to calculate area and perimeter of circle, square and rectangle based on user’s choice..c
@@ -1,16 +1,43 @@
 #include <stdio.h>
-int main() {
-    char c;
-    int lowercase_vowel, uppercase_vowel;
-    printf("Enter an alphabet: ");
-    scanf("%c", &c);
-    lowercase_vowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
-
-    uppercase_vowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
-
-    if (lowercase_vowel || uppercase_vowel)
-        printf("%c is a vowel.", c);
-    else
-        printf("%c is a consonant.", c);
+#include <stdlib.h>
+#include <math.h>
+int main()
+{
+    float area,radius,l,b,side,perimeter;
+    int choice;
+    printf("1. Circle\n");
+    printf("2. Rectangle\n");
+    printf("3. Square\n");
+    printf("Select your Choice\n");
+    scanf("%d",&choice);
+    switch(choice)
+    {
+    case 1:
+        printf("You have chosen Area and circumfrence of Circle\n");
+        printf("Enter the radius\n");
+        scanf("%f",&radius);
+        area=3.14*radius*radius;
+        perimeter=3.14*2*radius;
+        printf("Area of Circle is %.2f\nCircumfrence of Circle is %f\n",area, perimeter);
+        break;
+    case 2:
+        printf("You have chosen Area and perimeter of Rectangle\n");
+        printf("Enter length and breadth\n");//
+        scanf("%f%f",&l,&b);
+        area=(l*b);
+        perimeter=2*(l+b);
+        printf("Area of Rectangle= %.2f\nPerimeter of Rectangle= %f\n",area, perimeter);
+        break;
+    case 3:
+        printf("You have chosen Area and perimeter of Square\n");
+        printf("Enter value of side\n");
+        scanf("%f",&side);
+        area=side*side;
+        perimeter=4*side;
+        printf("Area of Square= %.2f\nPerimeter of Square= %f\n",area,perimeter);
+        break;
+    default:
+        printf("Sorry, Invalid Choice\n");
+    }
     return 0;
 }
